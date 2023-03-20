b871262b074f513c0e2d0f4b9a5a303607c55265
이로움 <roum02@jobkorea.co.kr>
2023-03-20 10:48:53 +0900

    (week1_7576) Fix bug - bfs 함수 실행 위치 변경 [roum02]

--- week1/7576/roum02/solution.py
+++ week1/7576/roum02/solution.py
@@ -24,29 +24,29 @@ queue = deque([])
 result = 0
 
 
-def bfs(y, x):
-    queue.append([y, x])
+def bfs():
     while queue:
         # 처음 토마토 좌표
         ey, ex = queue.popleft()
         for k in range(4):
             # 다음에 익힐 토마토의 좌표
             ny, nx = ey + dy[k], ex + dx[k]
-            print(nx, ny)
             # 좌표(박스) 크기를 넘어가면 안됨 and 해당 좌표 토마토가 익지 않은 상태(0)
             if 0 <= ny < n and 0 <= nx < m and box[ny][nx] == 0:
                 print("*****",nx, ny)
                 # 토마토를 익힌다 + 날짜 +1 -> 최대값 -1이 정답
                 box[ny][nx] = box[ey][ex] + 1
                 queue.append([ny, nx])
-                print("queue", queue)
 
 
 for j in range(n):
     for i in range(m):
         if box[j][i] == 1:
             # 맨 처음 토마토가 들어 있는 위치 넣어주기
-            bfs(j, i)
+            queue.append([j, i])
+
+# 이중 for 문 안에 함수를 실행시켜서, 토마토가 여러 개 있는 case에서 실행이 되지 못하였음!
+bfs()
 
 for i in box:
     for j in i:
