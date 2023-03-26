fc8f4a2bd3f949b6925fa91160187dc8cc2c8c6c
김형석 <gudtjr3437@jobkorea.co.kr>
2023-03-26 15:56:02 +0900

    (week2) 1439 [seok]

---
+++ week2/1439/seok/solution.py
@@ -0,0 +1,32 @@
+'''
+    분류 : 그리디
+    문제 이름 : 단어수학
+    문제 번호 : 1339
+    문제 링크 : https://www.acmicpc.net/problem/1339
+'''
+
+import sys
+
+
+def read():
+    return sys.stdin.readline().strip()
+
+
+command_line = int(read())
+alpha_dict = {}
+word_list = [read() for _ in range(command_line)]
+list_len = len(max(word_list, key=len))
+
+for text in word_list:
+    for index in range(len(text)):
+        num = 10 ** (len(text) - 1 - index)
+        alpha_dict[text[index]] = alpha_dict[text[index]] + \
+            num if text[index] in alpha_dict else num
+
+answer = 0
+sorted_list = sorted(alpha_dict.values(), reverse=True)
+
+for idx in range(len(sorted_list)):
+    answer += int(sorted_list[idx]) * (9 - idx)
+
+print(answer)
