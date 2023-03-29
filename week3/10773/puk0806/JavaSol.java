d2783f100cd7508d652579da466d5a6107765c43
박찬호 <puk0806@jobkorea.co.kr>
2023-03-29 17:31:32 +0900

    (week3) puk0806 10773 Java Sol [puk0806]

---
+++ week3/10773/puk0806/JavaSol.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int result = 0;
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            int m = sc.nextInt();
+            if (m == 0) {
+                stack.pop();
+            } else {
+                stack.add(m);
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            result += stack.pop();
+        }
+
+        System.out.println(result);
+
+    }
+}
+
+
+
+
+
+
