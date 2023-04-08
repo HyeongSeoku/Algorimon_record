1f3d886a9cc650080ae4dd70510ea897de7797c3
Marshall Ku <marshall@kakao.com>
2023-04-09 02:39:42 +0900

    Don't use native input()

--- week4/10610/marshallku/solution.py
+++ week4/10610/marshallku/solution.py
@@ -1,2 +1,2 @@
-i = sorted(list(input()), reverse=True)
+i = sorted(list(__import__('sys').stdin.readline().rstrip()), reverse=True)
 print(-1 if '0' not in i or sum(list(map(int, i))) % 3 != 0 else ''.join(i))
