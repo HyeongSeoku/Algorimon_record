52526460e4561dc9cbf745a500b655fdd6c7e21c
박찬호 <puk0806@naver.com>
2023-04-09 17:22:42 +0900

    (week4) puk0806 8983 Python Non Sol [puk0806]

---
+++ week4/8983/puk0806/solution.py
@@ -0,0 +1,47 @@
+"""
+풀이 :
+- 모든 동물들을 x축이 가까운 사로를 확인하며 사냥 가능한 동물의 수를 파악함
+
+놓친 부분
+- x축 좌표가 가까운 사로만 확인하면 됨
+"""
+
+
+import sys
+
+read = sys.stdin.readline
+
+m, n, l= map(int, read().split())
+shotList = list(map(int,read().split()))
+
+graph = []
+for i in range(n):
+    graph.append(list(map(int, read().split())))
+
+result = 0
+
+
+# print('m ',m)
+# print('n ',n)
+# print('l ',l)
+
+# print('shotList ',shotList)
+# print('graph ',graph)
+for i in range(len(graph)):
+    # if(l<graph[i][1]):continue
+
+    for j in range(len(shotList)):
+        # print('i : ',i)
+        # print('j : ',j)
+        # print('abs(graph[i][0]-shotList[j])+graph[i][1] ',abs(graph[i][0]-shotList[j])+graph[i][1])
+        if(l>= abs(graph[i][0]-shotList[j])+graph[i][1] ):
+            # print('증가@@@@@@@@@@@')
+            result +=1
+            break
+
+# print('result : ',result)
+print(result)
+
+
+
+
