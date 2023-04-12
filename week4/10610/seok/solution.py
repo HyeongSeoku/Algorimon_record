cafa8a87a22c32b568b4f149f0f3adf21a96dfc3
김형석 <gudtjr3437@naver.com>
2023-04-12 22:59:05 +0900

    (week4) 10610 README 수정 ✅ [seok]

--- week4/10610/seok/solution.py
+++ week4/10610/seok/solution.py
@@ -5,23 +5,13 @@
     문제 링크 : https://www.acmicpc.net/problem/10610
 '''
 
-
-'''
-끝자리는 무조건 0
-없으면 => -1
-
-모든 수들을 더해서 3으로 나눌수 있으면 30의 배수가 될 수 있음
-'''
-
 import sys
 read = sys.stdin.readline
 
 num_list = list(read().rstrip())
-if sum(map(int,num_list))%3 == 0 and '0' in num_list:
+if sum(map(int, num_list)) % 3 == 0 and '0' in num_list:
     # 불필요한 sort를 줄이는게 시간 단축의 키
     num_list.sort(reverse=True)
     print("".join(num_list))
 else:
     print(-1)
-
-
