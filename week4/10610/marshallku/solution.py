b4f7932bd0d923097e2d3fa50aa54bcc75d19b7a
Marshall Ku <marshall@kakao.com>
2023-04-13 16:49:27 +0900

    (week4) Simplify code of 10610

--- week4/10610/marshallku/solution.py
+++ week4/10610/marshallku/solution.py
@@ -1,2 +1,2 @@
-i = sorted(list(__import__('sys').stdin.readline().rstrip()), reverse=True)
-print(-1 if '0' not in i or sum(list(map(int, i))) % 3 != 0 else ''.join(i))
+i = sorted(__import__('sys').stdin.readline().rstrip(), reverse=True)
+print(-1 if '0' not in i or sum(map(int, i)) % 3 else ''.join(i))
