c49032da2a579f24f3b77a01098d71a93c309b88
이로움 <roum02@jobkorea.co.kr>
2023-04-16 17:32:53 +0900

    (week5) Add solution of 16173 [roum]

--- week5/16173/roum/solution.py
+++ week5/16173/roum/solution.py
@@ -1,43 +1,48 @@
 """
 점프왕 쩰리 ☄️
-
-아이디어
-1) 각 칸의 수를 stack에 넣는다
-2) 오른쪽/아래쪽으로 가보고 더 이상 갈 수 없으면 넘어간다
+1) BFS
 """
 
 import sys
+from collections import deque
 
 read = sys.stdin.readline
 
 n = int(read())
 matrix = [list(map(int, read().split(" "))) for _ in range(n)]
+visit_matrix = [[0] * n for _ in range(n)]
+
+dx = [1, 0]
+dy = [0, 1]
 
 
-def go_jelly(y, x):
-    # 쩰리가 위치한 수
-    position_num = matrix[y][x]
-    # 만약 출구에 도달했으면 종료
-    if position_num == -1:
-        return True
+def go_jelly(y, x, visited_matrix):
+    # 해당 좌표값 queue에 넣고 방문처리
+    queue = deque()
+    queue.append((y, x))
+    visited_matrix[y][x] = 1
 
-    # 오른쪽 또는 아래쪽의 좌표
-    nx = x + position_num
-    ny = y + position_num
-    # matrix 범위 안에 있어야만 실행
-    if nx < n and ny < n:
-        # 오른쪽으로 진행하기
-        value = go_jelly(y, nx)
-        if value:
-            go_jelly(y, nx)
-        else:
-            # 아래로 진행하기
-            go_jelly(ny, x)
-    # matrix 범위 안에 있지 않으면 실행 안함
-    else:
-        return False
+    while queue:
+        y, x = queue.popleft()
+        # 만약 출구에 도달했으면 종료
+        if matrix[y][x] == -1:
+            return 'HaruHaru'
+
+        # 오른쪽 또는 아래쪽의 좌표
+        for i in range(2):
+            nx = x + dx[i] * matrix[y][x]
+            ny = y + dy[i] * matrix[y][x]
+            # matrix 범위 안에 없으면 넘어가기
+            if nx >= n or ny >= n:
+                continue
+            # 이미 방문 했으면 넘어가기
+            if visited_matrix[ny][nx] == 1:
+                continue
+            # 방문을 하지 않았다면
+            if visited_matrix[ny][nx] == 0:
+                visited_matrix[ny][nx] = 1
+                queue.append((ny, nx))
+    return 'Hing'
 
 
-for j in range(n):
-    for i in range(n):
-        go_jelly(j, i)
+print(go_jelly(0, 0, visit_matrix))
