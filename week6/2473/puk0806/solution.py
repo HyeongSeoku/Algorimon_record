1769a05e6511c7f4c4d17982ed1a9c934a33b41a
박찬호 <puk0806@naver.com>
2023-04-21 18:41:58 +0900

    (week6) 2473 Python Non Sol Time Out [puk0806]

--- week6/2473/puk0806/solution.py
+++ week6/2473/puk0806/solution.py
@@ -1,4 +1,10 @@
 import sys
+import bisect
+
+"""
+정렬후 2개에를 묶고 나머지에 대해서 2진탐색 진행
+여전히 느림 ...
+"""
 
 
 def input():
@@ -14,12 +20,23 @@ result = []
 
 for i in range(0, size-2, 1):
     for j in range(i+1, size-1, 1):
-        for z in range(j+1, size, 1):
-            temp_sum = ph_list[i]+ph_list[j]+ph_list[z]
+        temp_sum = ph_list[i]+ph_list[j]
+        temp_ph_list = ph_list[j+1:]
 
-            if (abs(zero_sum) > abs(temp_sum)):
-                zero_sum = temp_sum
-                result = [ph_list[i], ph_list[j], ph_list[z]]
+        ph_index = bisect.bisect_left(temp_ph_list, -temp_sum)
+
+        if (ph_index >= len(temp_ph_list)):
+            ph_index = ph_index-1
+        elif (ph_index < 0):
+            ph_index = 0
+        else:
+            if (abs(temp_sum+temp_ph_list[ph_index-1]) < abs(temp_sum+temp_ph_list[ph_index])):
+                ph_index -= 1
+
+        temp_sum += temp_ph_list[ph_index]
+        if (abs(zero_sum) > abs(temp_sum)):
+            zero_sum = temp_sum
+            result = [ph_list[i], ph_list[j], temp_ph_list[ph_index]]
 
 
 result.sort()
