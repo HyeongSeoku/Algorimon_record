3b636c4603f4bc11a1fa852004c27fb4cf2f8680
이로움 <roum02@jobkorea.co.kr>
2023-04-24 00:32:39 +0900

    (week6) Add solution 20551 - binary search, sequential search [roum]

---
+++ week6/20551/roum/solution2.py
@@ -0,0 +1,35 @@
+"""
+sort 마스터 배지훈의 후계자
+=> 이진탐색 + 중복값이 있을 경우의 case 추가 필요
+"""
+
+import sys
+
+read = sys.stdin.readline
+N, M = map(int, read().split(" "))
+B = sorted([int(read()) for _ in range(N)])
+D = [int(read()) for _ in range(M)]
+
+
+def binary_search(array, target, start, end):
+    if start > end:
+        return None
+    mid = (start + end) // 2
+    if array.count(mid) > 1:
+        mid = array.index(mid)
+
+    # 1) 중간값이 타겟값일 경우
+    if array[mid] == target:
+            return mid
+    # 2) 중간값이 타겟값보다 클 경우
+    elif array[mid] > target:
+        return binary_search(array, target, start, mid - 1)
+    # 3) 중간값이 타겟값보다 작을 경우
+    else:
+        return binary_search(array, target, mid + 1, end)
+
+
+for d in D:
+    result = binary_search(B, d, 0, N - 1)
+    result = -1 if not isinstance(result, int) else result
+    print(result)
